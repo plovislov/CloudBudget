34338a89a9184492e314492ce274ba03db448960
plovislov <plovislov@gmail.com>
2020-03-19 15:43:51 +0300

    Menu

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -2,8 +2,10 @@
 
 let button = document.querySelector('.js-nav-button');
 let menu = document.querySelector('.js-menu');
+let body = document.querySelector('body');
 
 button.addEventListener('click', function () {
     this.classList.toggle('opened');
     menu.classList.toggle('opened');
+    body.classList.toggle('menu-opened');
 });
